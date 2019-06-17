4904a02b3f3585d302ad1a798cdd8750b84fdcd0
qintao29 <1925217820@qq.com>
2019-06-17 11:47:31 +0800

    1

---
+++ app/src/main/java/cn/edu/gdpt/myapplication/CONFIG.java
@@ -0,0 +1,8 @@
+package cn.edu.gdpt.myapplication;
+
+public class CONFIG {
+    public class API{
+        final static  String URL="http://apis.juhe.cn/simpleWeather/query?city=%E8%8B%8F%E5%B7%9E&key=946191298dbf4c92ac1f93293376990b";
+
+    }
+}
---
+++ app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
@@ -0,0 +1,89 @@
+package cn.edu.gdpt.myapplication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+import okhttp3.Call;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        
+    }
+
+    private void initData() {
+        String url=CONFIG.API.URL;
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.d("TAG","请求成功"+response);
+                        processData(response);
+                    }
+                });
+    }
+    private void processData(String json){
+        JsonBean jsonBean= JSON.parseObject(json,JsonBean.class);
+        String cityNmae=jsonBean.getResult().getCity();
+        Log.d("tt","城市名:"+cityNmae);
+        String citytemperature=jsonBean.getResult().getRealtime().getTemperature();
+        Log.d("cc","最低温度："+citytemperature+"℃");
+        String cityhumidity=jsonBean.getResult().getRealtime().getHumidity();
+        Log.d("cx","最高温度："+cityhumidity+"℃");
+        String cityinfo=jsonBean.getResult().getRealtime().getInfo();
+        Log.d("tx","今天天气情况为："+cityinfo);
+
+    }
+}
