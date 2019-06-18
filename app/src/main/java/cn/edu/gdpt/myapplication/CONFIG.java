08510fabdafe146edeaf8f9c101f6aa5c838f2bf
吕钦涛 <46305874+qinTao29@users.noreply.github.com>
2019-06-18 23:40:27 +0800

    start

--- app/src/main/java/cn/edu/gdpt/myapplication/CONFIG.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.gdpt.myapplication;
-
-public class CONFIG {
-    public class API{
-        final static  String URL="http://apis.juhe.cn/simpleWeather/query?city=佛山&key=946191298dbf4c92ac1f93293376990b";
-
-    }
-}
--- app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
@@ -18,9 +18,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.alibaba.fastjson.JSON;
-import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.StringCallback;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -42,7 +39,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
 
-import okhttp3.Call;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,46 +47,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initData();
-
-    }
-
-    private void initData() {
-        String url=CONFIG.API.URL;
-        OkHttpUtils
-                .get()
-                .url(url)
-                .build()
-                .execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
-
-                    }
-
-                    @Override
-                    public void onResponse(String response, int id) {
-                        Log.d("TAG","请求成功"+response);
-                        processData(response);
-                    }
-                });
-    }
-    private void processData(String json){
-        JsonBean jsonBean= JSON.parseObject(json,JsonBean.class);
-        //当天天气情况
-        String cityNmae=jsonBean.getResult().getCity();
-        Log.d("tt","城市名："+cityNmae);
-        String citytemperature=jsonBean.getResult().getRealtime().getTemperature();
-        Log.d("cc","温度："+citytemperature+"℃");
-        String cityhumidity=jsonBean.getResult().getRealtime().getHumidity();
-        Log.d("cx","湿度："+cityhumidity);
-        String cityinfo=jsonBean.getResult().getRealtime().getInfo();
-        Log.d("tx","天气情况："+cityinfo);
-        String citydirect=jsonBean.getResult().getRealtime().getDirect();
-        Log.d("fx","风向："+citydirect);
-        String citypower=jsonBean.getResult().getRealtime().getPower();
-        Log.d("fs","风速："+citypower);
-        //未来天气情况
 
 
     }
+
 }
