1e8625921a6a18589949065988fa1f42819233b7
吕钦涛 <46305874+qinTao29@users.noreply.github.com>
2019-06-19 00:02:35 +0800

    splashactivity引导页

--- app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
@@ -1,43 +1,8 @@
 package cn.edu.gdpt.myapplication;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.ActivityCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/cn/edu/gdpt/myapplication/SplashActivity.java
@@ -0,0 +1,31 @@
+package cn.edu.gdpt.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        init();
+    }
+
+    private void init() {
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,2000);
+    }
+}
