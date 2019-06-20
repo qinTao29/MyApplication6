88d6015e687bf89343752e0b08bdae2abf1801c4
qintao29 <1925217820@qq.com>
2019-06-20 15:04:57 +0800

    MainActivity 实现接口数据的显示 实现定位功能

--- app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/MainActivity.java
@@ -1,15 +1,27 @@
 package cn.edu.gdpt.myapplication;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,10 +61,164 @@ public class MainActivity extends AppCompatActivity {
         getLocation();
         getWeather();
     }
+    //网络请求，获取天气预报
     private void getWeather() {
+        //创建okHttpClient对象
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        //创建一个Request
+        FormEncodingBuilder builder = new FormEncodingBuilder();
+        if (cityName != null) {
+            builder.add("city", cityName);
+        }
+        builder.add("key", "946191298dbf4c92ac1f93293376990b");
+
+        Request request = new Request.Builder()
+                .url("http://apis.juhe.cn/simpleWeather/query")
+                .post(builder.build())
+                .build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                try {
+                    final String result = response.body().string();
+                    Gson gson = new Gson();
+                    final WeatherResponse weatherResponse = gson.fromJson(result, WeatherResponse.class);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+                                WeatherResul.WeatherRealTime weatherRealTime = weatherResponse.getResult().getRealtime();
+                                city_tv.setText(weatherResponse.getResult().getCity());
+                                info_tv.setText("天气状况："+weatherRealTime.getInfo());
+                                temperature_tv.setText("温度："+weatherRealTime.getTemperature() + "℃");
+                                humidity_tv.setText("湿度："+weatherRealTime.getHumidity());
+                                direct_tv.setText("风向："+weatherRealTime.getDirect());
+                                power_tv.setText("风力："+weatherRealTime.getPower());
+                                futuresList = weatherResponse.getResult().getFuture();
+                                weatherAdapter = new WeatherAdapter(MainActivity.this, futuresList);
+                                future_listview.setAdapter(weatherAdapter);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
 
     }
+    //获取当前位置经纬度
     private void getLocation(){
+// 获取系统服务
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // 网络定位
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = locationManager
+                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
 
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+        //定位成功
+        if (null != location) {
+            gecodLocation();
+        }
+    }
+    //解析经纬度
+    private void gecodLocation() {
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        //创建一个Request
+        FormEncodingBuilder builder = new FormEncodingBuilder();
+        builder.add("lng", location.getLongitude() + "");
+        builder.add("lat", location.getLatitude() + "");
+        builder.add("key", "3bf7a00ae635e911ee92dbf668b7ac3c");
+        builder.add("type", 1 + "");
+
+        Request request = new Request.Builder()
+                .url("http://apis.juhe.cn/geo/")
+                .post(builder.build())
+                .build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                try {
+                    final String result = response.body().string();
+                    Gson gson = new Gson();
+                    final LocationResponse locationResponse = gson.fromJson(result, LocationResponse.class);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+                                String cityStr = locationResponse.getResult().getAddress();
+                                if(cityStr.contains("市")){
+                                    int index = cityStr.indexOf("市");
+                                    String cityTemp = cityStr.substring(index-2,index);
+                                    cityName = cityTemp;
+                                }
+                                getWeather();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (null != data) {
+                cityName = data.getStringExtra("city");
+                getWeather();
+            }
+        }
     }
 }
