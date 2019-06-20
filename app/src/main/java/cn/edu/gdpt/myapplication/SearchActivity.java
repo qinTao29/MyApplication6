e05df7b07a7c5ddea96ba912cce7a7a0f9725ab7
suyangkai <1090320670@qq.com>
2019-06-20 14:35:57 +0800

    TextView EditText Button 控件初始化

--- app/src/main/java/cn/edu/gdpt/myapplication/SearchActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/SearchActivity.java
@@ -2,12 +2,23 @@ package cn.edu.gdpt.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+private TextView back_tv;
+private EditText city_name_edit;
+private Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        back_tv=(TextView)findViewById(R.id.back_tv);
+        city_name_edit=(EditText)findViewById(R.id.city_name_edit);
+        searchBtn=(Button)findViewById(R.id.search_btn);
+        //返回
+
     }
 }
