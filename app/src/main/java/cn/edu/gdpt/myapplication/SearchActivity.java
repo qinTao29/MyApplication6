ebcb0b4503bd96ac7acfc2f17e37b24146062da3
suyangkai <1090320670@qq.com>
2019-06-20 14:38:27 +0800

    设置监听

--- app/src/main/java/cn/edu/gdpt/myapplication/SearchActivity.java
+++ app/src/main/java/cn/edu/gdpt/myapplication/SearchActivity.java
@@ -1,11 +1,13 @@
 package cn.edu.gdpt.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
 private TextView back_tv;
@@ -23,6 +25,20 @@ private Button searchBtn;
         back_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
+            }
+        });
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = city_name_edit.getText().toString().trim();
+                if (name.isEmpty()) {
+                    Toast.makeText(SearchActivity.this, "城市不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent();
+                intent.putExtra("city",name);
+                setResult(1,intent);
                 finish();
             }
         });
